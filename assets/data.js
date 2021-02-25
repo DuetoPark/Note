d98bd907b919060e0871cde22c0a66e3a208b182
DuetoPark <ffdddffddd11@gmail.com>
2021-02-25 17:16:10 +0900

    DOC: state.네비게이션 data 작성 -assets/data.js

---
+++ assets/data.js
@@ -0,0 +1,32 @@
+let state = {
+  네비게이션: {
+    html: [
+      {id: "01", href: "meta"},
+      {id: "02", href: "pre"},
+      {id: "03", href: "inline-block"},
+      {id: "04", href: "picture"},
+      {id: "05", href: "list-child-element"},
+    ],
+    css: [
+      {id: "01", href: "link"},
+      {id: "02", href: "stroke"},
+      {id: "03", href: "background-clip"},
+      {id: "04", href: "line-break"},
+      {id: "05", href: "pseudo-opacity"},
+      {id: "06", href: "width"},
+      {id: "07", href: "main-tag-IE"},
+      {id: "08", href: "transform-origin"},
+      {id: "09", href: "padding-top"},
+      {id: "10", href: "direction"},
+      {id: "11", href: "텍스트숨기기"},
+    ],
+    javascript: [
+      {id: "01", href: "pseudo-style"},
+      {id: "02", href: "toggle"},
+      {id: "03", href: "copy"},
+      {id: "04", href: "e.target"},
+      {id: "05", href: "switch"},
+      {id: "06", href: "timing-event"},
+    ],
+  },
+};
