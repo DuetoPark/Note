4a4e8d5f32428ff072a9d91a710d1b7b40fcea89
DuetoPark <ffdddffddd11@gmail.com>
2021-02-27 23:27:19 +0900

    CLN: modal.js 리팩토링 -assets/app/modal

--- assets/app/modal.js
+++ assets/app/modal.js
@@ -1,62 +1,52 @@
 // main.html
 // Go To Page Modal
-const goToPages = document.querySelector(".go-to-pages");
+const goToPages = document.querySelector("#go-to-pages");
 let navList = document.querySelector(".note-header-nav-list");
 const goToPageDiv = document.querySelectorAll(".go-to-page-div");
 const close = document.querySelector(".go-to-pages-close");
-let menuText = null;
+let menuText = "";
+let navKeys = Object.keys(state.네비게이션);
 
-
+// 모달 열기 함수
 let openMenuModal = function (target) {
-  switch (menuText) {
-    case "html":
-      goToPageDiv[0].classList.remove("show-element");
-      break;
-    case "css":
-      goToPageDiv[1].classList.remove("show-element");
-      break;
-    case "javascript":
-      goToPageDiv[2].classList.remove("show-element");
-      break;
-  }
-
   menuText = target.textContent;
-  goToPages.classList.add("modal-active");
 
-  switch (menuText) {
-    case "html":
-      goToPageDiv[0].classList.add("show-element");
+  goToPages.classList.remove("hidden");
+  goToPages.classList.add("go-to-pages");
 
-      break;
-    case "css":
-      goToPageDiv[1].classList.add("show-element");
-      break;
-    case "javascript":
-      goToPageDiv[2].classList.add("show-element");
-      break;
+  for (let i=0; i<goToPageDiv.length; i+=1) {  // 모든 모달 내용 숨기기
+    goToPageDiv[i].classList.add("hidden");
+  }
+
+  if (navKeys.indexOf(menuText) > -1) { // 클릭한 주제의 모달 내용만 보이기
+    goToPageDiv[navKeys.indexOf(menuText)].classList.remove("hidden");
   }
 }
 
+// 모달 닫기 함수
 let closeMenuModal = function (target) {
-  goToPages.classList.add("modal-inactive");
+  goToPages.classList.add("modal-close");
+
   window.setTimeout(function () {
-    goToPages.classList.remove("modal-active");
-  }, 200);
-  window.setTimeout(function () {
-    goToPages.classList.remove("modal-inactive");
+    goToPages.classList.remove("modal-close", "go-to-pages");
+    goToPages.classList.add("hidden");
+    for (let i=0; i<goToPageDiv.length; i+=1){
+      goToPageDiv[i].classList.add("hidden");
+    }
   }, 200);
 }
 
-
-function openMenuHandler (e) {
-  if (e.target.tagName === "BUTTON") {
+// 모달 열기 핸들러
+function goToPagesHandler (e) {
+  if (navKeys.indexOf(e.target.textContent) > -1) {
     openMenuModal(e.target);
   }
 }
 
+// 모달 닫기 핸들러
 function closeMenuHandler () {
   closeMenuModal();
 }
 
-navList.addEventListener("click", openMenuHandler);
+navList.addEventListener("click", goToPagesHandler);
 close.addEventListener("click", closeMenuHandler);
