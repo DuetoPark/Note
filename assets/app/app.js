99167d2c651245ca50b445d8553e6db7e0861c50
DuetoPark <ffdddffddd11@gmail.com>
2021-03-23 00:19:08 +0900

    CLN: 야간 모드 전환 이벤트 리팩토링 -assets/app/app

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,81 +1,36 @@
-// Toggle ClickEvent
+// 야간 모드로 전환 이벤트
+const body = document.body;
 const toggleButton = document.querySelector('.toggle-button');
-const toggleButtonAfter = document.querySelector('.toggle-button::after');
-const typeElements = document.querySelectorAll('.type');
-const backgroundWhiteElements = document.querySelectorAll('[class*=background-white]');
-let transparentButton = document.querySelectorAll('.transparent-button');
+const transparentButtons = document.querySelectorAll('.transparent-button');
 const pageMenus = document.querySelectorAll('.note-header-nav-list-items > button');
 const goToPageAnchors = document.querySelectorAll('.go-to-page-anchor');
-const noticeState = document.querySelector("#page-mode-state");
+const pageModeState = document.querySelector("#page-mode-state");
 let current = null;
 
+function classListToggle() {
+  body.classList.toggle('background-toggled');
+  toggleButton.classList.toggle('button-toggled');
+  pageMenus.forEach(pageMenu => pageMenu.classList.toggle('menu-toggled'));
+  transparentButtons.forEach(button => button.classList.toggle('transparent-toggled'));
+
+  pageModeState.textContent = !current ? "야간 모드 켜짐" : "야간 모드 꺼짐";
+}
 function toggled () {
-  document.body.classList.add('background-toggled');
-  toggleButton.classList.add('new-style');
-  for (let i=0; i<pageMenus.length; i++) {
-    pageMenus[i].classList.add('menu-toggled');
-  }
-  for (let i=0; i<goToPageAnchors.length; i++) {
-    goToPageAnchors[i].style.color = "#000";
-  }
-
-  current = document.body.classList.value;
-  toggleButton.style.background = '#FFFFFF';
-
-  if(typeElements.length>0) {
-    for (let i=0; i<typeElements.length; i++) {
-      typeElements[i].style.color = '#1F1F1F';
-    }
-  }
-
-  if(backgroundWhiteElements.length>0) {
-    for (let i=0; i<backgroundWhiteElements.length; i++) {
-      backgroundWhiteElements[i].style.color = '#1F1F1F';
-    }
-  }
-
-  if(transparentButton.length>0) {
-    for (let i=0; i<transparentButton.length; i++) {
-      transparentButton[i].classList.add('toggled-button');
-    }
-  }
-
-  noticeState.textContent = "야간 모드 켜짐";
+  current = "켜짐";
+  classListToggle();
 }
-
-function toggleInactive () {
-  document.body.classList.remove('background-toggled');
-  toggleButton.classList.remove('new-style');
-
-  for (let i=0; i<pageMenus.length; i++) {
-    pageMenus[i].classList.remove('menu-toggled');
-  }
-
+function isNotToggled() {
   current = null;
-  toggleButton.style.background = '#525252';
-
-  for (let i=0; i<transparentButton.length; i++) {
-    transparentButton[i].classList.remove('toggled-button');
-  }
-
-  noticeState.textContent = "야간 모드 꺼짐";
+  classListToggle();
 }
-
-
-let toggleHandler = function () {
-  if (current === null) {
-    toggled();
-  } else {
-    toggleInactive();
-  }
+function toggleHandler() {
+  !current ? toggled() : isNotToggled();
 }
-
 toggleButton.addEventListener('click', toggleHandler);
 
 
 // Copy ClickEvent
 let noteMain = document.querySelector('.note-main');
-
 
 function copyData (elem) {
   let dummy = document.createElement('textarea');
