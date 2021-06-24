1b1ebd4d96f689fe00db20790513e31e1c6845d0
DuetoPark <inhye.park.dev@gmail.com>
2021-06-24 10:41:22 +0900

    CLN: toggle.js 리팩토링

--- src/assets/app/toggle.js
+++ src/assets/app/toggle.js
@@ -1,33 +1,22 @@
-// 야간 모드로 전환 이벤트
 const body = document.body;
 const toggleButton = document.querySelector('.toggle-button');
-const transparentButtons = document.querySelectorAll('.transparent-button');
-const pageMenus = document.querySelectorAll(
-  '.note-header-nav-list-items > button'
-);
-const goToPageAnchors = document.querySelectorAll('.go-to-page-anchor');
-const pageModeState = document.querySelector('#page-mode-state');
-let current = null;
+const alertMode = document.querySelector('#page-mode-state');
 
-function classListToggle() {
-  body.classList.toggle('background-toggled');
-  toggleButton.classList.toggle('button-toggled');
-  pageMenus.forEach((pageMenu) => pageMenu.classList.toggle('menu-toggled'));
-  transparentButtons.forEach((button) =>
-    button.classList.toggle('transparent-toggled')
-  );
+function dayMode() {
+  return body.getAttribute('class') === 'mode-day';
+}
 
-  pageModeState.textContent = !current ? '야간 모드 켜짐' : '야간 모드 꺼짐';
+function setBodyClass() {
+  body.setAttribute('class', dayMode() ? 'mode-night' : 'mode-day');
 }
-function toggled() {
-  current = '켜짐';
-  classListToggle();
+
+function changeAlert() {
+  alertMode.textContent = dayMode() ? '야간 모드 켜짐' : '야간 모드 꺼짐';
 }
-function isNotToggled() {
-  current = null;
-  classListToggle();
-}
+
 function toggleHandler() {
-  !current ? toggled() : isNotToggled();
+  setBodyClass();
+  changeAlert();
 }
+
 toggleButton.addEventListener('click', toggleHandler);
